3847d493440118fc1a333d49e9df3aa63290b0ff
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-10-01 05:46:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,79 @@
 
-from fastapi import FastAPI, Form, Request
-from fastapi.responses import HTMLResponse
+# FASTAPI Imports
+from fastapi import FastAPI, Request, Header, Form
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from fastapi.encoders import jsonable_encoder
+# Other Imports
+from uuid import uuid4
+from typing import Annotated, Union
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 items = []
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get('/todos', response_class=HTMLResponse)
-async def list_items(request: Request,):
-    return templates.TemplateResponse('todos.html', {'request': request, 'items': items})
+@app.get("/todos", response_class=HTMLResponse)
+async def list_todos(request: Request, hx_request: Annotated[Union[str, None], Header()] = None):
+    if hx_request:
+        return templates.TemplateResponse("todos.html", {"request": request, "todos": items})
+    return JSONResponse(content=jsonable_encoder(items))
 
 
-@app.post('/todos', response_class=HTMLResponse)
-async def create_list(request:Request, name: str = Form(...)):
-    items.append(name)
-    return templates.TemplateResponse('todos.html', {'request': request, 'items': items})
+@app.post("/todos", response_class=HTMLResponse)
+async def create_todo(request: Request, todo: Annotated[str, Form(...)]):
+    items.append(TODO(todo))
+    return templates.TemplateResponse("todos.html", {"request": request, "todos": items})
 
+
+@app.put("/todos/{todo_id}", response_class=HTMLResponse)
+async def update_todo(request: Request, todo_id: str, text: Annotated[str, Form()]):
+    for index, todo in enumerate(items):
+        if str(todo.id) == todo_id:
+            todo.text = text
+            break
+    return templates.TemplateResponse(request=request, name="todos.html", context={"todos": items})
+
+
+@app.post("/todos/{todo_id}/toggle", response_class=HTMLResponse)
+async def toggle_todo(request: Request, todo_id: str):
+    for index, todo in enumerate(items):
+        if str(todo.id) == todo_id:
+            items[index].done = not items[index].done
+            break
+    return templates.TemplateResponse(
+        request=request, name="todos.html", context={"todos": items}
+    )
+
+
+@app.post("/todos/{todo_id}/delete", response_class=HTMLResponse)
+async def delete_todo(request: Request, todo_id: str):
+    for index, todo in enumerate(items):
+        if str(todo.id) == todo_id:
+            del items[index]
+            break
+    return templates.TemplateResponse(
+        request=request, name="todos.html", context={"todos": items}
+    )
+    
+# Todo Model
+
+
+class TODO:
+    def __init__(self, text: str):
+        self.id = uuid4()
+        self.text = text
+        self.done = False
 
 
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run('main:app',host='0.0.0.0', reload=True)
+
 
