acb6d022fab800b33e7cd34fe90fb1324bfbad8f
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-10-01 01:34:07 +0600

    Update and rename test.py to main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory='temp1')
+items = []
+
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+@app.get('/todos', response_class=HTMLResponse)
+async def list_items(request: Request,):
+    return templates.TemplateResponse('todos.html', {'request': request, 'items': items})
+
+
+@app.post('/todos', response_class=HTMLResponse)
+async def create_list(request:Request, name: str = Form(...)):
+    items.append(name)
+    return templates.TemplateResponse('todos.html', {'request': request, 'items': items})
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('main:app', port=8002, reload=True)
+
