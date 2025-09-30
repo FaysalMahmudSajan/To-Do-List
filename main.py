6ad4c00febd066f0ea46e1ac9eccefb8a60c5224
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2025-10-01 02:18:59 +0600

    upload

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ async def create_list(request:Request, name: str = Form(...)):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run('main:app',host='0.0.0.0', port=8002, reload=True)
+    uvicorn.run('main:app',host='0.0.0.0', reload=True)
 
